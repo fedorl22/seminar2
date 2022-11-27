6d4642c59423e80e018150127e345e64c581fda0
Fedor <fdr@mail.ru>
2022-11-27 10:57:39 +0300

    add hw10 range task_10

--- seminar2/task_10/Program.cs
+++ seminar2/task_10/Program.cs
@@ -7,8 +7,16 @@
 
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int number1 = number % 10;
-int number2 = ((number -number1) % 100)/10; 
+if (number > 999 || number < 100)
+{
+    Console.WriteLine("Вы ввели цифру не из указанного диапазона.");
+}
+else
+{
+    int number1 = number % 10;
+    int number2 = ((number - number1) % 100) / 10;
 
     Console.WriteLine(number2);
+}
+
 
